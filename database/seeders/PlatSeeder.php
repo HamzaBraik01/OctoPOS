c776e29c877482d3b1de6111b2a81459873f6042
HamzaBraik01 <hamzabraik02@gmail.com>
2025-04-26 18:58:40 +0100

    Ajout de 10 nouveaux plats et mise à jour des seeders

--- database/seeders/PlatSeeder.php
+++ database/seeders/PlatSeeder.php
@@ -14,7 +14,7 @@ class PlatSeeder extends Seeder
                 'nom' => 'Poulet rôti',
                 'description' => 'Poulet rôti aux herbes',
                 'prix' => 90.50,
-                'menu_id' => 1, // Correspond au menu "Menu Classique"
+                'menu_id' => 1, // Correspond au "Menu Classique"
                 'categorie' => 'Plats',
                 'image' => 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 'created_at' => now(),
@@ -24,7 +24,7 @@ class PlatSeeder extends Seeder
                 'nom' => 'Salade Veggie',
                 'description' => 'Salade de légumes frais',
                 'prix' => 45.00,
-                'menu_id' => 2, // Correspond au menu "Menu Végétarien"
+                'menu_id' => 2, // Correspond au "Menu Végétarien"
                 'categorie' => 'Entrées',
                 'image' => 'https://images.pexels.com/photos/2097090/pexels-photo-2097090.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 'created_at' => now(),
@@ -34,7 +34,7 @@ class PlatSeeder extends Seeder
                 'nom' => 'Tiramisu',
                 'description' => 'Dessert italien à base de mascarpone',
                 'prix' => 60.00,
-                'menu_id' => 1, // Correspond au menu "Menu Classique"
+                'menu_id' => 3, // Correspond au "Menu Enfant"
                 'categorie' => 'Desserts',
                 'image' => 'https://images.pexels.com/photos/852506/pexels-photo-852506.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 'created_at' => now(),
@@ -44,7 +44,7 @@ class PlatSeeder extends Seeder
                 'nom' => 'Coca Cola',
                 'description' => 'Boisson gazeuse rafraîchissante',
                 'prix' => 20.00,
-                'menu_id' => 1, // Correspond au menu "Menu Classique"
+                'menu_id' => 4, // Correspond au "Menu Découverte"
                 'categorie' => 'Boissons',
                 'image' => 'https://images.pexels.com/photos/1256331/pexels-photo-1256331.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 'created_at' => now(),
@@ -54,7 +54,7 @@ class PlatSeeder extends Seeder
                 'nom' => 'Vin Rouge',
                 'description' => 'Vin rouge de Bordeaux',
                 'prix' => 150.00,
-                'menu_id' => 2, // Correspond au menu "Menu Végétarien"
+                'menu_id' => 5, // Correspond au "Menu Gourmand"
                 'categorie' => 'Vins',
                 'image' => 'https://images.pexels.com/photos/2071421/pexels-photo-2071421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 'created_at' => now(),
@@ -64,7 +64,7 @@ class PlatSeeder extends Seeder
                 'nom' => 'Menu du Jour',
                 'description' => 'Entrée + Plat + Dessert',
                 'prix' => 120.00,
-                'menu_id' => 1, // Correspond au menu "Menu Classique"
+                'menu_id' => 6, // Correspond au "Menu Express"
                 'categorie' => 'Menu du jour',
                 'image' => 'https://images.pexels.com/photos/7788453/pexels-photo-7788453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
                 'created_at' => now(),
